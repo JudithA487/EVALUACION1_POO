2307379586241db4456e3312e379af12d7858815
JudithA487 <143568356+JudithA487@users.noreply.github.com>
2024-03-01 19:38:57 -0600

    Add files via upload

---
+++ EVA_1_1_CLASES_Y_OBJETOS/src/eva_1_1_clases_y_objetos/EVA_1_1_CLASES_Y_OBJETOS.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_1_clases_y_objetos;
+
+/**
+ *
+ * @author acost
+ */
+public class EVA_1_1_CLASES_Y_OBJETOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Persona perso1;
+        //solo eh declarado la variable, no eh creado el objeto
+        //crear clase (intanciar) consiste en asignar memoria.
+        int valor;
+    
+    }
+    
+}
+
+//clase persona
+//NombreDeLaClase --- Nomenglatura Camelcase
+    class Persona{
+    
+   
+}
+    }
+    
+}
