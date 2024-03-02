94baac69e5217840d9a497402c2f81d25e11cf34
JudithA487 <143568356+JudithA487@users.noreply.github.com>
2024-03-01 19:38:04 -0600

    Add files via upload

---
+++ EVA1_5_CLASE_PERSONA/src/eva1_5_clase_persona/EVA1_5_CLASE_PERSONA.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_5_clase_persona;
+
+/**
+ *
+ * @author acost
+ */
+public class EVA1_5_CLASE_PERSONA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Persona perso1 = new Persona();
+        
+    }
+    
+}
+
+class Persona{
+    //ATRIBUTOS ---> ESTADO DEL OBJETO
+    //NO DEBEN DE SER ACCESIBLE DIRECTAMENTE
+    private String nombre;
+    private String apellidos;
+    private int edad;
+    
+    //METODOS:
+    //PUBLICOS --> SON LA INTERFAZ
+    //SI SON PARA MANIPULAR LOS ATRIBUTOS AY UN ESTANDAR
+    //EN JAVA
+    //METODOS SET Y GET
+}
